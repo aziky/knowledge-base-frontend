886a95182a597c93ee971d73429d40d6b9c428c7
aziky <pcm230304@gmail.com>
2025-10-27 13:48:36 +0700

    init source

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ArrowUpIcon } from 'lucide-react'
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button variant="outline">Button</Button>
+        <Button variant="outline" size="icon" aria-label="Submit">
+          <ArrowUpIcon />
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
